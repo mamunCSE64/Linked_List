cac6c96b7e46838fefa676e86ce86bdb0a5b05a3
Abdul Al Mamun <124201619+mamunCSE64@users.noreply.github.com>
2025-02-13 11:32:55 +0600

    Create Delete_N'th_positon.cpp

---
+++ Delete_N'th_positon.cpp
@@ -0,0 +1,125 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define pb push_back
+#define pob pop_back
+#define pf push_front
+#define pof pop_front
+#define gcd __gcd
+#define int_string to_string
+#define string_int stoi
+#define mn(v) *min_element(v.begin(), v.end())
+#define mx(v) *max_element(v.begin(), v.end())
+#define index_character s.find('character')
+#define countxchar count(s.begin(), s.end(), 'x')
+#define index_ofX_vector find(v.begin(), v.end(), x) - v.begin()
+#define point cout << fixed << setprecision(10)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define n1 cout << "-1" << endl
+#define ok cout << "ok" nl
+#define sorted is_sorted(v.begin(), v.end())
+#define nl << endl
+#define sp << " "
+#define mp make_pair
+#define fi first
+#define se second
+#define inf LLONG_MAX
+#define mod %1000000007
+// ordered_set
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp> 
+using namespace __gnu_pbds;   
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> 
+// bitsize-> ll size=log2(n)+1 (last set bit=size-1;/ /Last bit->__lg(value); )
+// first bit-> name._Find_First()
+// next bit after x-> name._Find_next()
+// freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+// BesidesDuplicateCharacterEraseInString s.erase(unique(s.begin(), s.end()), s.end());
+// Upper/lower-> transform(s.begin(), s.end(), s.begin(), ::toupper/tolower);
+
+ll i, j, k, n, m, e=1e9;
+const ll N = 2e6+5;
+double pi=acos(-1);
+bool cmp(pair<ll,ll> a,pair<ll,ll> b){
+    if(a.fi==b.fi) return (a.se>b.se); return (a.fi>b.fi);}
+
+//
+// Don't get stuck on a single approach for long, think of multiple ways
+// You will destroy your dream if you stay in your comfort zone
+// **********************|| Main Code ||********************************
+//
+ 
+//  g++ test.cpp -o a && ./a
+//  g++ -o a test.cpp && ./a
+//  g++ -o a test.cpp && a.e
+class node{
+    public:
+    int data;
+    node *next;
+};
+
+int main()
+{ 
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+    // point;
+    ll test = 1; 
+    // cin >> test;    
+    again: 
+    while (test--)
+    {   
+        int n;
+        cin>>n;
+        vector<ll> ar(n);
+        for(i=0;i<n;i++){
+            cin>>ar[i];
+        }
+        int N,value; cin>>N>>value;
+        node *head,*current;
+        head=current=NULL;
+        for(i=0;i<n;i++){
+            node *tmp;
+            tmp=new node();
+            tmp->data=ar[i];
+            tmp->next=NULL;
+            if(head==NULL){
+                head=tmp;
+                current=tmp;
+            }else{
+                current->next=tmp;
+                current=tmp;
+            }
+        }
+        node *x=head;
+        cout << "before deletion : " nl;
+        while(x!=NULL){
+            cout << x->data sp;
+            x=x->next;
+        }
+        cout nl;
+        // Insert into the N'th position,value
+        node *tmp=head;
+        int count=0;
+        while(1){            
+            count++;
+            if(count==N-1){
+                node *extra=new node();
+                extra->data=value;
+                extra->next=tmp->next;
+                tmp->next=(tmp->next)->next;
+                delete extra;
+                break;
+            }
+            tmp=tmp->next;
+        }
+        cout << "after deletion : " nl;
+        tmp=head;
+        while(tmp!=NULL){
+            cout << tmp->data sp;
+            tmp=tmp->next;
+        }
+        cout nl;
+    }
+     
+}
